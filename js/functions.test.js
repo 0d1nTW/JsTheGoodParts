92089aa170ce050fdc07eab449a6d7d43bfac108
asalasan <asalasan@touchpointmed.com>
2018-11-26 17:59:12 +0200

    Added functions.test.js for unit testing the functions

--- js/functions.test.js
+++ js/functions.test.js
@@ -6,4 +6,18 @@ describe('Our first test', ()=> {
     })
 })
 
+describe('Arithmetical operations test', ()=> {
+    it('Add function test', ()=> {
+        expect(add(3,4)).to.equal(7);
+    })
+
+    it('Subtract function test', ()=> {
+        expect(sub(3,4)).to.equal(-1);
+    })
+
+    it('Subtract function test', ()=> {
+        expect(mul(3,4)).to.equal(12);
+    })
+})
+
 
